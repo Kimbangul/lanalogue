b6dabff94dc25cf8ce534a4f56daa449c79ccc91
Kimbangul <highcolor_12@naver.com>
2023-07-05 15:50:06 +0900

    chore: add tsparticles

---
+++ src/components/tsparticle/Particle.tsx
@@ -0,0 +1,11 @@
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
+const Particle = () => {
+  return(
+    <></>
+  )
+}
+
+export default Particle;
