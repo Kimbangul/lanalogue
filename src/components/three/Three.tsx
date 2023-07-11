2361ed6acaed0f77c9f42ea01fd1ef1390252495
Kimbangul <highcolor_12@naver.com>
2023-07-12 08:26:50 +0900

    style: model edit

--- src/components/three/Three.tsx
+++ src/components/three/Three.tsx
@@ -1,33 +1,39 @@
 import * as THREE from 'three';
 import { Canvas, Vector3, useFrame, useThree, useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls, useGLTF, Clone } from '@react-three/drei';
 
 const ThreeObj = () => {
   const {scene} = useGLTF('/scene.gltf');
-  return <Clone object={scene} castShadow/>
+  return <Clone object={scene} castShadow />
 }
 
 const ThreeView = () => {
   const pos : Vector3 = [3,3,3];
   return (
     <Canvas dpr={window.devicePixelRatio} shadows={true}>
-      <OrbitControls 
-        autoRotate={true} 
-        enableZoom={true}
-        autoRotateSpeed={0.4}
-      />
-      <mesh >
-      <ambientLight intensity={0.4} color='#dd913a' />
-      <directionalLight intensity={1} position={[0,0, -20]}/>
-      
-      <pointLight color="indianred" intensity={0.1}/>
-      <pointLight position={[10, 10, -10]} color="orange" intensity={0.3}/>
-      <pointLight position={[-10, -10, 10]} color="pink" intensity={0.2}/>
-      <ThreeObj />
-      {/* <dodecahedronGeometry/>
-      <meshStandardMaterial roughness={0.4} emissive="#404057" /> */}
-      </mesh>
+      <group>
+        <OrbitControls 
+          autoRotate={true} 
+          enableZoom={true}
+          autoRotateSpeed={0.4}
+        />
+        <spotLight
+          color={[0, 0.5, 1]}
+          intensity={2}
+          angle={0.1}
+          penumbra={0.5}
+          position={[0, 0, 0]}
+          castShadow
+        />
+        <mesh >
+          <ambientLight intensity={0.7} color='#ffdfa5' position={[-10,0,10]}/>
+          <ambientLight intensity={0.7} color='#a6ffe9' position={[10,0,10]}/>
+          <pointLight intensity={0.5} color='#fff' position={[0,0,0]}/>
+          <directionalLight intensity={1} position={[0,0,100]}/>
+          <ThreeObj />
+          <meshStandardMaterial emissive={[0.5, 0.5, 0.5]} color={[255, 255, 255]} />
+        </mesh>
+      </group>
     </Canvas>
   )
 }
