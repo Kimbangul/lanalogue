b8010c15887b169f193f8274986ad11921e79a06
Kimbangul <highcolor_12@naver.com>
2023-07-13 11:15:18 +0900

    style: edit model

--- src/components/three/Three.tsx
+++ src/components/three/Three.tsx
@@ -6,7 +6,7 @@ import { Vector3 } from 'three';
 
 const ThreeObj = () => {
   const {scene} = useGLTF('/scene.gltf');
-  return <primitive object={scene} castShadow emissive={'#fff'} emissiveIntensity={2}/>
+  return <primitive object={scene} castShadow emissive={new THREE.Color('lime')} emissiveIntensity={2}/>
 }
 
 function Rig() {
@@ -30,23 +30,16 @@ const ThreeView = () => {
           enableZoom={true}
           autoRotateSpeed={0.2}
           rotateSpeed={0.4}
+          // rotation={[0,0,0]}
         />
-        <spotLight
-          color={[0, 0.5, 1]}
-          intensity={2}
-          angle={0.1}
-          penumbra={0.5}
-          position={[0, 0, 0]}
-          castShadow
-        />
+        <ambientLight intensity={0.3} color='#ffdfa5' position={[-10,0,10]}/>
+        <ambientLight intensity={0.2} color='#a6ffe9' position={[10,0,10]}/>
+        <pointLight intensity={0.2} color='#ffdfa5' position={[0,0,0]}/>
+        <directionalLight intensity={1} position={[0,0,100]}/>
         <PerspectiveCamera />
         <mesh ref={ref}>
-          <ambientLight intensity={0.3} color='#ffdfa5' position={[-10,0,10]}/>
-          <ambientLight intensity={0.2} color='#a6ffe9' position={[10,0,10]}/>
-          <pointLight intensity={5} color='#ffdfa5' position={[0,0,0]}/>
-          <directionalLight intensity={3} position={[0,0,100]}/>
           <ThreeObj />
-          <meshStandardMaterial emissive={'#fff'} emissiveIntensity={5} />
+          <meshStandardMaterial  />
         </mesh>
         <Rig />
       </group>
